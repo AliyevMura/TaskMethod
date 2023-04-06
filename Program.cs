2615e78ecbc3c2b5215309d8dfa0478380dcef82
AliyevMura <7lvghl9@code.edu.az>
2023-04-06 08:37:02 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿namespace TaskMethod
+{
+    internal class Program
+
+    {
+
+        //Task 1
+        //static void Main()
+        //{
+            
+        //    Console.WriteLine(Area(10.0));
+        //    Console.WriteLine(Area(7,8));
+        //    Console.WriteLine(Area(6, 7, 8));
+
+        //    Console.WriteLine(Area(2,3,4,5));
+           
+        //}
+        //static double Area(double r,int p=3)
+        //{
+        //    return p * r * r;
+        //}
+
+        //static int Area(int a, int b)
+        //{
+        //    return a * b;
+        //}
+
+
+
+        //static int Area(int a, int b, int c)
+        //{
+        //    return 2*(a * b + a * c + b * c);
+
+        //}
+
+        //static int  Area(int a, int b, int c, int r)
+        //{
+        //    int p = (a + b + c) / 2;
+        //    int s = p * r;
+        //    return s;
+
+
+        //}
+
+
+
+       // Task2
+
+        //burda nese eksine gedir
+        //static void Main()
+        //{
+        //    Console.WriteLine(Math(3,2));
+        //}
+        //static int Math(int num)
+        //{
+        //    return num * num;
+        //}
+        //static int Math(int num, int time)
+        //{
+
+        //    while (time > 0)
+        //    {
+        //        num = num * num;
+        //        time--;
+        //    }
+        //    return num;
+        //}
+
+        
+
+    }
+}
